91e353f7712afcb544f4648d74c9d58591d81eb0
Raven Hughes <ravenhillh@gmail.com>
2023-08-16 22:03:02 +0000

    describe commit

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -10,25 +10,24 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
 */
-function fizzBuzz() {
-    // YOUR CODE GOES BELOW HERE //
-//writing a for loop to count from 1 to 100
-for (var i = 1; i < 101; i ++){
-    //if number is a multiple of both three and five logging FizzBuzz    
-    if (i % 3 == 0 && i % 5 == 0){
-        console.log("FizzBuzz")
-    //if a number is a multiple of three logging Fizz    
-    } else if (i % 3 == 0){
-        console.log("Fizz")
-    //if number is a multiple of five logging Buzz    
-    }else if  (i % 5 == 0){
-        console.log("Buzz")
-    //else just logging the number
-    else console.log i;
-}                
 
+    // YOUR CODE GOES BELOW HERE //
+//declaring my function fizzBuzz
+const fizzBuzz = function(){
+//writing a for loop to count from 1 to 100
+for (var i = 1; i < 101; i++){
+//writing an if-else if statement to print each output correctly
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz")
+    } else if (i % 3 === 0){
+        console.log("Fizz")
+    } else if (i % 5 === 0){
+        console.log("Buzz")
+    } else console.log(i)
     // YOUR CODE GOES ABOVE HERE //
 }
+ } 
+
 
 
 
