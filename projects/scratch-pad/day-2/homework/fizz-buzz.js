0f9d16064195aac339c98bdc6b7561843d38ae0d
Raven Hughes <ravenhillh@gmail.com>
2023-08-16 02:18:39 +0000

    describe commit

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,8 +12,20 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
-
-
+//writing a for loop to count from 1 to 100
+for (var i = 1; i < 101; i ++){
+    //if number is a multiple of both three and five logging FizzBuzz    
+    if (i % 3 == 0 && i % 5 == 0){
+        console.log("FizzBuzz")
+    //if a number is a multiple of three logging Fizz    
+    } else if (i % 3 == 0){
+        console.log("Fizz")
+    //if number is a multiple of five logging Buzz    
+    }else if  (i % 5 == 0){
+        console.log("Buzz")
+    //else just logging the number
+    else console.log i;
+}                
 
     // YOUR CODE GOES ABOVE HERE //
 }
