08f39e4ab4be1e0eaddb52904852c41e82770c0a
Raven Hughes <ravenhillh@gmail.com>
2023-08-23 22:54:40 +0000

    describe commit

--- Studies/variables.js
+++ Studies/variables.js
@@ -1,7 +1,26 @@
 /**
  * 
- * VARIABLES:
- * 0.
+ * VARIABLES: 
+ * 
+ * 0. Variables are used to record data and attach it to a label to have access to that data.
+ * 
  * 1.
  * 2.
  */
+
+//Declaration
+//Declaration is when you create a variable and name it, but without assigning it a value.
+var myDog
+console.log(myDog) //prints undefined, because variable has no value yet
+
+//Initialization or Assignment
+//Assignment or initialization is when you assign a value to your variable.
+
+var myDog = "Burt"
+console.log(myDog) //prints Burt to the console
+
+//Re-assignment
+//When you change the value of a variable.
+
+var myDog = "Sally"
+console.log(myDog) // logs Sally to the console 
