1e2754200516a7120d9a8c96ae286f6b131bab51
Raven Hughes <ravenhillh@gmail.com>
2023-08-26 22:24:39 +0000

    describe commit

--- projects/eloquent-javascript/chapter-02/exercises.js
+++ projects/eloquent-javascript/chapter-02/exercises.js
@@ -4,15 +4,44 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 function triangles() {
-
+//Write a loop that makes seven calls to console.log to output the following triangle
+//make a string variable to hold the values to be printed
+var triangle = ""
+//make a loop to generate and print the values
+//make a string var to represent the input number
+let abc = "abc"
+for (var i = 0; i < abc.length; i ++){
+  //make a statement to add values to variable a
+  triangle += "#";
+  console.log(triangle)
+  //make a statement to log the variable
+}
 }
 ////////////////////////////////////////////////////////////////////////////////
 // fizzBuzz ////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////
 
 function fizzBuzz() {
-  
-}
+ /*Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. 
+ For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3),
+  print "Buzz" instead.
+When you have that working, modify your program to print "FizzBuzz" for numbers that are divisible by both 3 and 5 
+and still print "Fizz" or "Buzz" for numbers divisible by only one of those). */
+
+  //writing a for loop to count from 1 to 100
+  for (var i = 1; i <= 100; i++){
+  //writing an if-else if statement to print each output correctly
+      if (i % 3 === 0 && i % 5 === 0){
+          console.log("fizzbuzz")
+      } else if (i % 3 === 0){
+          console.log("fizz")
+      } else if (i % 5 === 0){
+          console.log("buzz")
+      } else console.log(i)
+      // YOUR CODE GOES ABOVE HERE //
+  }
+   } 
+
 
 ////////////////////////////////////////////////////////////////////////////////
 // drawChessboard //////////////////////////////////////////////////////////////
