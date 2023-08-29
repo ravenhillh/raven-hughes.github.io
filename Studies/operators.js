fdff960d564a1497203910fd9a2050fdfcd0d69e
Raven Hughes <ravenhillh@gmail.com>
2023-08-29 22:21:23 +0000

    describe commit

--- Studies/operators.js
+++ Studies/operators.js
@@ -1,12 +1,16 @@
 /**
  * 
- * OPERATORS: s
+ * OPERATORS: 
+ * 
  * 0. Operators are tools we use in javascript to relate two pieces of data, 0r compare them
  * or assign one to a variable. There are many different types of operators. Most require two operands
  * to work with, but some only one or three.
- * 1. Assignment operators
- * 2. Arithmatic operators
- * 3. Comparison operator
+ * 
+ * 1. Assignment operators assign the value on the right side to the variable on the left side.
+ * They can also add, multipy, subtract or divide the variable.
+ * 2. Arithmatic operators are used to do math on numbers, such as adding or subtracting them.
+ * 3. Comparison operator are used to compare two values, to see if they're equal or compare how 
+ * they are different. Sometimes they compare strictly or loosely.
  * 4. Logical operators
  * 5. Unary operators
  * 6. Ternary operators
