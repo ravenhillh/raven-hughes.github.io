9d78156dc6944ca1cc2a367f6e4be878eab8638f
Raven Hughes <ravenhillh@gmail.com>
2023-09-02 15:32:24 +0000

    describe commit

--- Studies/operators.js
+++ Studies/operators.js
@@ -2,16 +2,43 @@
  * 
  * OPERATORS: 
  * 
- * 0. Operators are tools we use in javascript to relate two pieces of data, 0r compare them
+ * 0. Operators are tools we use in javascript to relate two pieces of data, or compare them
  * or assign one to a variable. There are many different types of operators. Most require two operands
- * to work with, but some only one or three.
+ * to work with, but some one or three.
  * 
  * 1. Assignment operators assign the value on the right side to the variable on the left side.
  * They can also add, multipy, subtract or divide the variable.
  * 2. Arithmatic operators are used to do math on numbers, such as adding or subtracting them.
  * 3. Comparison operator are used to compare two values, to see if they're equal or compare how 
- * they are different. Sometimes they compare strictly or loosely.
- * 4. Logical operators
- * 5. Unary operators
- * 6. Ternary operators
+ * they are different. Sometimes they compare strictly or loosely. They return a boolean value.
+ * 4. Logical operators are include &&, || and !, they are used to relate two expressions which will be resolved to a boolean
+ * value.
+ * 5. Unary operators are operators with only one operand. This operand can sometimes come before or after the operand.
+ * 6. Ternary operator is the only operator in javascript that takes three operands. It begins with a condition and then a
+ * question mark, and then an expression to execute if the conditions is truthy, then a colon and a final expression to 
+ * execute if the condition is falsy.
  */
+
+//Assignment operators
+var myCat = "buddy"
+console.log(myCat) // prints buddy to the console
+
+//Arithmatic operators
+var num = 3 - 2
+console.log(num) //prints 1 to the console
+
+//Comparison operators
+if (10 > 5){
+    console.log("true") // prints true to the console because the condition resolved to true
+}
+//Logical operators
+if (3 > 1 && a > d){
+    console.log('true') // prints the console because both expression resolved to true
+}
+//Unary operators
+let a = 1;
+let b = a++;
+console.log(b) // logs 2 to the console 
+
+//Ternary operator
+array[i] === 'bob' ? true : false; // would print true if array[i] was equal to bob and false if not
--- Studies/string-manipulation.js
+++ Studies/string-manipulation.js
@@ -1,7 +1,35 @@
 /**
  * 
  * STRING-MANIPULATION:
- * 0.
- * 1. (INCLUDE FOUR METHODS)
- * 2.
+ * 0. String manipulation relates to diffent methods we use to modify a string value. A string in javascript is any bit of 
+ * text that we wrap within quotation marks. They can include characters, numbers and Unicode. There are many functions and methods
+ * that are built in to javascript to modify strings. Strings are immutable so when modifying the string you have to create
+ * a new version of the string with the changes.
+ * 
+ * 1. You can manipulate a string with operators. For example, you can add two strings together with the "+" sign. This is 
+ * called string concatenation.
+ * 
+ * 2.With string methods you can use built in methods to manipulate the string into something new. Some methods take the string
+ * and change it into an array, such as the split method. Some methods can check for the contents of a string, such as the
+ * includes method, or the endsWith method.
  */
+
+//String manipulation with operators
+let string1 = "Sup";
+let string2 = " Dawg";
+console.log(string1 + string2) // prints to the console Sup Dawg
+
+//String manipulation with string methods
+//1. String manipulation with .split
+let string3 = "Hey there";
+let array1 = string3.split(",");
+console.log(array1) // prints [H, e, y, t, h, e, r, e]
+
+//2. String manipulation with .padStart
+let string = "Raven".padStart(10, 0)
+console.log(string) // prints to the console 00000Raven
+
+//3. String manipulation with .toUpperCase
+let word = "computer";
+let up = word[0].toUpperCase();
+console.log(up) // prints C to the console
