44ba95ca3d84565af6ff0a3b84bb4c79c3c8169d
Raven Hughes <ravenhillh@gmail.com>
2023-09-09 23:35:18 +0000

    describe commit

--- projects/recursion-practice/src/recursion.js
+++ projects/recursion-practice/src/recursion.js
@@ -369,7 +369,18 @@ var flatten = function(arrays) {
 
 // 30. Given a string, return an object containing tallies of each letter.
 // letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
-var letterTally = function(str, obj) {
+var letterTally = function(str, obj = {}) {
+  //base case
+  if (str.length === 0){
+    return obj
+  }
+  //recursion
+  if (obj.hasOwnProperty(str[0])){
+    obj[str[0]] += 1
+  } else {
+    obj[str[0]] = 1
+  }
+  return letterTally(str.slice(1, str.length), obj)
 };
 
 // 31. Eliminate consecutive duplicates in a list.  If the list contains repeated
@@ -377,7 +388,17 @@ var letterTally = function(str, obj) {
 // elements should not be changed.
 // Example: compress([1, 2, 2, 3, 4, 4, 5, 5, 5]) // [1, 2, 3, 4, 5]
 // Example: compress([1, 2, 2, 3, 4, 4, 2, 5, 5, 5, 4, 4]) // [1, 2, 3, 4, 2, 5, 4]
-var compress = function(list) {
+var compress = function(list, newList = []) {
+  //base - when list is gone
+  if (list.length === 0){
+    return newList
+  }
+  //recursion
+  if (newList.includes(list[0])){
+  } else {
+    newList.push(list[0])
+  }
+  return compress(list.slice(1, list.length), newList)
 };
 
 // 32. Augment every element in a list with a new value where each element is an array
