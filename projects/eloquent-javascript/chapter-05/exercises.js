f9edf9cb5f1abbb82e3db94525075cb73109eb07
Raven Hughes <ravenhillh@gmail.com>
2023-09-14 02:02:02 +0000

    describe commit

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -1,18 +1,38 @@
 // /////////////////////////////////////////////////////////////////////////////
 // flatten /////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
-
-function flatten() {
-
+//Use the reduce method in combination with the concat method to “flatten” an array of arrays into a single array
+// that has all the elements of the original arrays.
+function flatten(array) {
+  newArray = array.reduce(function(acc, curr){
+acc.push(curr)
+return acc
+  }, [])
+  
+let goodArray = newArray[0].concat(newArray[1], newArray[2])
+  return goodArray
 }
+		
+
 
 // /////////////////////////////////////////////////////////////////////////////
 // loop ////////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
+//Write a higher-order function loop that provides something like a for loop statement. It takes a value, 
+//a test function, an update function, and a body function. Each iteration, it first runs the test function 
+//on the current loop value and stops if that returns false. Then it calls the body function, giving it 
+//the current value. Finally, it calls the update function to create a new value and starts from the beginning.
 
-function loop() {
-
+function loop(value, test, update, body) {
+  //loop thru input values
+for (let i = value ; test(i); i = update(i)){
+  body(i);
+  //update the value to create a new value
+  
 }
+}
+
+
 
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
