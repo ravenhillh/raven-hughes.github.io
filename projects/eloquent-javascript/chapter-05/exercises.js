a34e45fb9f0e24255070f32026a4e16c4bf988c1
Raven Hughes <ravenhillh@gmail.com>
2023-09-15 18:59:43 +0000

    describe commit

--- projects/eloquent-javascript/chapter-05/exercises.js
+++ projects/eloquent-javascript/chapter-05/exercises.js
@@ -37,9 +37,17 @@ for (let i = value ; test(i); i = update(i)){
 // /////////////////////////////////////////////////////////////////////////////
 // every ///////////////////////////////////////////////////////////////////////
 // /////////////////////////////////////////////////////////////////////////////
-
-function every() {
-
+/*Analogous to the some method, arrays also have an every method. This one returns true when the given function 
+returns true for every element in the array. In a way, some is a version of the || operator that acts on arrays, 
+and every is like the && operator.
+Implement every as a function that takes an array and a predicate function as parameters.*/
+function every(array, test) {
+for (let i = 0; i < array.length; i ++){
+  if(test(array[i]) === false){
+    return false
+  }
+}
+return true
 }
 
 // /////////////////////////////////////////////////////////////////////////////
