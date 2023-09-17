52fcb7ae167df9c81d021ce510f2c68982cd2947
Raven Hughes <ravenhillh@gmail.com>
2023-09-17 23:21:48 +0000

    describe commit

--- Studies/loops.js
+++ Studies/loops.js
@@ -18,9 +18,44 @@
  * at each property in the given object. The loop ends after hitting each property.
  * 
  * 4. You can loop to any point, going forward, by creating a stopping point with the stopping condition, and iterating
- * forward with i + 1. You can loop backwards starting at any point with the starting condition of i a given number,
- * and stopping condition of i > 0, and using i -1 to increment backwards.
+ * forward with i + 1. You can loop backwards starting at any point with the starting condition of given number,
+ * and stopping condition of i > 0, and using i -- to increment backwards.
  * 
- * 5. Loop over an Array, forwards and backwards
-Loop over an Object
+ * 5. Loop over an Array forwards by starting at 0, assigning i to 0, and having the loop stop at array.length. 
+ * You can loop backwards setting the starting variable at array.length-1, and setting the end condition at i => 0, and
+ * using a negative to decrement the counter, i --. You can loop over an object using a for- in loop
  */
+
+//While loop
+let x = 0
+while (x < 5){
+    console.log(x) // logs 0, 1, 2, 3, 4 to the console one at a time
+    x ++
+}
+
+//For loop going forwards
+for (let i = 0; i < 5; i ++){ // logs 0, 1, 2, 3, 4 to the console one at at time
+    console.log(i)
+}
+//For loop going backwards
+for (let i = 5; i >= 0; i --){
+    console.log(i)         // logs 5, 4, 3, 2, 1, 0 to the console
+}
+
+//For-in loop with object
+let cat = {isTabby: true, eyeColor: "yellow"}
+for (var key in cat){
+    console.log(cat[key]) //logs true and yellow to the console
+}
+
+//Loop forward over an array
+let arr = [1, 2, 3]
+
+for (let i = 0; i < arr.length; i ++){
+    console.log(arr[i])     //prints 1, 2, 3 to the console
+}
+
+//loop backward over an array
+for (let i = arr.length -1; i >= 0; i --){
+    console.log(arr[i])      // logs 3, 2, 1 to the console
+}
