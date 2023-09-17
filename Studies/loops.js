4bd329e9bbe88aa429a0f9167ab75c4d4ad11d66
Raven Hughes <ravenhillh@gmail.com>
2023-09-17 21:34:45 +0000

    describe commit

---
+++ Studies/loops.js
@@ -0,0 +1,26 @@
+/**
+ * 
+ * LOOPS: Loops are a way to repeat one piece of code over and over a set number of times. There are many different 
+ * types of loops, but they basically do the same thing, repeat a piece of code a certain amount of times. Different 
+ * types of loops provide different ways to start and stop the looping process. Each of these different loop types
+ * are more suited for particular situations. With a loop you can either iterate through a data structure, such as an
+ * object or an array, or you can just use the index number of the loop to do something at each index.
+ * 
+ * 1. While loops are used to loop while a certain condition is true, once the condition resolves to false the 
+ * loop will stop iterating. You can also use a counter n to increment and decide where to start and stop the 
+ * iteration.
+ * 
+ * 2. For loops iterate for a certain number of times, until it meets the stopping condition. It uses an incrementer
+ * to move the loop and for each index it executes the code block. Once the stopping condition resolves to true the 
+ * loop stops. They can iterate through an array or just count and do something at each index.
+ * 
+ * 3. For-in loops are used to iterate through an object, using a specified key and object. It runs the block of code
+ * at each property in the given object. The loop ends after hitting each property.
+ * 
+ * 4. You can loop to any point, going forward, by creating a stopping point with the stopping condition, and iterating
+ * forward with i + 1. You can loop backwards starting at any point with the starting condition of i a given number,
+ * and stopping condition of i > 0, and using i -1 to increment backwards.
+ * 
+ * 5. Loop over an Array, forwards and backwards
+Loop over an Object
+ */
