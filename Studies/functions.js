bd9d2ca0e171533f9c9f40798823e56e6db3f036
Raven Hughes <ravenhillh@gmail.com>
2023-09-18 02:53:07 +0000

    describe commit

---
+++ Studies/functions.js
@@ -0,0 +1,15 @@
+/**
+ * FUNCTIONS: 
+ * 
+ * The two phases to using functions: First we must ___? Next we can execute (or two other words for executing a 
+ * function?) a function by?
+ * What’s the difference between a function’s parameters and arguments PASSED to a function?
+ * What’s the syntax for a NAMED function?
+ * How do we assign a function to a variable?
+ * Functions can OPTIONALLY take inputs, and OPTIONALLY return a single value. How do we specify inputs, and how do
+ * we specify outputs?
+ * Scope: Functions can see and modify variables in parent or global scopes. The inverse is NOT true.
+ * Closures: Functions form closures around the data they house. If an object returned from the Function and 
+ * is held in memory somewhere (referenced), that closure stays ALIVE, and data can continue to exist in these 
+ * closures! 
+ */
