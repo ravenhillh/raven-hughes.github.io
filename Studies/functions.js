fade55ba365c91888891abcaa70832fdf98fe34e
Raven Hughes <ravenhillh@gmail.com>
2023-09-19 03:28:56 +0000

    describe commit

--- Studies/functions.js
+++ Studies/functions.js
@@ -1,13 +1,31 @@
 /**
  * FUNCTIONS: 
+ * A function is a piece of code designed to perform a particular task. First you must define a function with the 
+ * keyword function, then name the function, and then put parentheses, in which you can place parameters. Next you
+ * place the code block within curly braces, this is where you write your code that will perform the function's task.
+ * Now that you have created the function or defined it, you can invoke or call the function. This is done by
+ * writing the name of the function followed by parentheses, which will hold the arguments you are passing in
+ * to the function.
  * 
  * The two phases to using functions: First we must ___? Next we can execute (or two other words for executing a 
  * function?) a function by?
- * What’s the difference between a function’s parameters and arguments PASSED to a function?
- * What’s the syntax for a NAMED function?
- * How do we assign a function to a variable?
- * Functions can OPTIONALLY take inputs, and OPTIONALLY return a single value. How do we specify inputs, and how do
- * we specify outputs?
+ * 
+ * 1. When you define a function you create how ever many parameters it may need, inside the parentheses, these are 
+ * basically placeholders, which are named so that you have an idea what the function input it, such as num1 and num2.
+ * When you invoke a function you pass in the arguments that you would like to use as input for your function, they
+ * are going to be used within the function, it could be numbers or a string or object, or even another function.
+ * 
+ * 2. A named function is written by first putting the function keyword and then the name of the function, then
+ * parentheses, with some or no parameters, and then curly braces, in which your code block will go.
+ * 
+ * 3. If we want to assign a function to a variable we can just say let or const and the name of the function is 
+ * equal to function and then put our parentheses, with parameters, and then put curly braces, with the code block
+ * inside.
+ * 
+ * 4. Functions can take inputs, they are passed in as arguments to the function. Functions can return a value, this
+ * is done with a return statement at the end of our code block, which specifies what will be returned.
+ * 
+ * 5. Scope can exist at the function level or at the global level. 
  * Scope: Functions can see and modify variables in parent or global scopes. The inverse is NOT true.
  * Closures: Functions form closures around the data they house. If an object returned from the Function and 
  * is held in memory somewhere (referenced), that closure stays ALIVE, and data can continue to exist in these 
